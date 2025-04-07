c364e4cd323ce98f24a845e176ad12a2ed544e1e
Evett <pwrammerfors@yahoo.com>
2025-04-07 01:39:17 -0500

    Deleting irrelevant stuff

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import { Preloader } from './scenes/Preloader';
-import { Play } from './scenes/Play';
 import { MenuScene } from './scenes/MenuScene';
 import { MapScene } from './scenes/MapScene';
 import { BattleScene } from './scenes/BattleScene';
@@ -30,9 +28,7 @@ const config = {
         BossScene,
         AltarScene,
         DeckScene,
-        CharacterSelectScene,
-        Preloader,
-        Play
+        CharacterSelectScene
     ]
 };
 
