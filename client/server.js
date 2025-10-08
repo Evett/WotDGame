1b694a7f48012fc83203da270ab8a0935879f784
Evett <pwrammerfors@yahoo.com>
2025-10-08 01:57:55 -0500

    adding server.js

---
+++ client/server.js
@@ -0,0 +1,16 @@
+import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
